f7ad7be30486f21c6ce0c6edeefe50cd172c0611
Kaung Myat Tun <kaungtun111@gmail.com>
2022-10-29 15:27:39 +0630

    User Interface section

---
+++ src/components/TransactionsList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TransactionsList = () => {
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        transaction
+      </ul>
+    </>
+  )
+}
+
+export default TransactionsList
